bbe284b287f7fe90aed82e37e8da2d451f04d09b
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-03-26 12:51:47 +0800

    feat: ESLint, Prettier, some packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  devServer: {
+    host: '127.0.0.1',
+    port: 1007,
+  },
+  modules: [
+    '@pinia/nuxt',
+    '@nuxt/image',
+    '@pinia-plugin-persistedstate/nuxt',
+    'dayjs-nuxt',
+    'nuxt-icon',
+    'nuxt-typed-router',
+    '@nuxtjs/color-mode',
+  ],
 })
