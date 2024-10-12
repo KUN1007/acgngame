29997eea251c3597c94db3b8f6dc385456c8dde1
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-10-12 16:28:42 +0800

    mod: eslint, nuxt-icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+
+  compatibilityDate: '2024-10-12',
+
   devServer: {
     host: '127.0.0.1',
     port: 3007
   },
+
   modules: [
-    '@pinia/nuxt',
     '@nuxt/image',
+    '@nuxt/icon',
+    '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
-    'nuxt-icon',
     '@nuxtjs/color-mode',
     'dayjs-nuxt'
   ],
 
   // Frontend
   css: ['~/assets/css/index.scss'],
+
   vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler'
+        }
+      }
+    },
     esbuild: {
       drop: ['console', 'debugger']
     },
@@ -26,6 +38,7 @@ export default defineNuxtConfig({
       }
     }
   },
+
   piniaPersistedstate: {
     cookieOptions: {
       maxAge: 60 * 60 * 24 * 7,
